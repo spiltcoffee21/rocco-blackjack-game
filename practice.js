a3ee01695401632e5a487663e4b45587548ded76
spiltcoffee21 <140124277+spiltcoffee21@users.noreply.github.com>
2025-08-19 14:59:39 +1000

    makeing the base of the deck in java script having a dicshonary and list of cards and suits help with remy and charlie

--- practice.js
+++ practice.js
@@ -1,2 +1,38 @@
-var button = document. ;
+function CreateDeck(){
+var suits = ["Hearts", "dimonds", "clubs", "spades"]
+var values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
+var deck = [];
 
+    for (var suit of suits) {
+        for ( var rank of ranks) {
+            let value;
+            if (rank ==="jack" || rank === "queen" || rank === "king") {
+                value = 10;
+            }else if (rank === "ace") {
+                value = 11;
+            }else {
+                value = parseInt(rank);
+            }
+        
+            deck.push({
+                suit: suit,
+                rank: rank,
+                value: value
+            });
+        }
+    }
+    return deck;
+}
+var mydeck = CreateDeck();
+console.log(mydeck);
+
+
+
+
+
+
+
+
+
+
+        
