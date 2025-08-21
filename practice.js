40cb6a4e4f0d2a40a97d8bd0ce3f3af12f856f7f
spiltcoffee21 <140124277+spiltcoffee21@users.noreply.github.com>
2025-08-21 12:38:37 +1000

    got a shuffle function  to shuffle an indervidule deck

--- practice.js
+++ practice.js
@@ -1,14 +1,14 @@
 function CreateDeck(){
-var suits = ["Hearts", "dimonds", "clubs", "spades"]
-var values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
+var suits = ["Hearts", "Dimonds", "Clubs", "Spades"]
+var ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
 var deck = [];
 
     for (var suit of suits) {
         for ( var rank of ranks) {
             let value;
-            if (rank ==="jack" || rank === "queen" || rank === "king") {
+            if (rank ==="Jack" || rank === "Queen" || rank === "King") {
                 value = 10;
-            }else if (rank === "ace") {
+            }else if (rank === "Ace") {
                 value = 11;
             }else {
                 value = parseInt(rank);
@@ -20,6 +20,14 @@ var deck = [];
                 value: value
             });
         }
+    }
+    return deck;
+}
+
+function Shuffle(deck) {
+    for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1)); 
+        [deck[i], deck[j]] = [deck[j], deck[i]];
     }
     return deck;
 }
